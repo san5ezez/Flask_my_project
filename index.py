299cfbba8e176b3d8e9be2522f9559bcad1442b4
san5ezez <dexter.craft.boss@gmail.com>
2023-04-14 20:16:39 +0600

    test cooooommit

---
+++ index.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, request, redirect
+from data import data, kaunt
+import random
+app = Flask(__name__)
+
+
+# Создаем переменные с карточками
+sherif_1 = 'Шериф'
+sherif_2 = 'Шериф'
+doctor = 'Доктор'
+mir_1 = 'Мирный'
+mir_2 = 'Мирный'
+all_cards = [sherif_1, sherif_2, doctor, mir_1, mir_2]
+
+# Маршрут для отображения шаблона и обработки формы
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Получаем выбранную карточку из формы
+        player_card = request.form['card']
+        # Выбираем случайным образом карточку компьютера
+        computer_card = random.choice(all_cards)
+        # Определяем победителя
+        if player_card == 'Мафия':
+            result = 'Вы проиграли! Компьютер выбрал карту ' + computer_card
+        elif player_card == computer_card:
+            result = 'Ничья! Компьютер выбрал ту же карту: ' + computer_card
+        else:
+            result = 'Вы выиграли! Компьютер выбрал карту ' + computer_card
+                # Возвращаем результат и обновляем страницу
+        return render_template('index.html', result=result)
+    else:
+    # Если метод запроса GET, отображаем шаблон без результата
+        return render_template('index.html')
+    
+@app.route('/delete_card', methods=['POST'])
+def delete_card():
+    id = request.form['id']
+    for card in data:
+        print (str(id) )
+        if str(card['id']) == str(id):
+            data.remove (card)
+    return redirect ('/admin')
+
+@app.route('/add_player', methods=['POST'])
+def add_ployer():
+    pl = {}
+    pl["id"] = kaunt
+    pl["player_role"] = request.form["player_role"]
+    data.append(pl)
+    print(pl)
+    print(data)
+    return redirect('/admin')
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html', data=data)
+
+if __name__ == '__main__':
+    app.run (debug=True)
